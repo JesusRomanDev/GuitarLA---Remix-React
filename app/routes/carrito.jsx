4870c71440165809a2ec2a11aa89f9d37ded06db
Jesus Roman <jesus_fime@outlook.es>
2023-11-01 19:11:23 -0600

    Modifying Cart Route and CSS for Cart.jsx

--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import styles from '../styles/carrito.css'
+
+export function links(){
+    return[
+        {rel: 'stylesheet', href: styles}
+    ]
+}
+
+export function meta(){
+    return[
+        {title: 'GuitarLA - Carrito de Compras'},
+        {description: 'Carrito de GuitarLA'}
+    ]
+}
 
 function Carrito() {
   return (
-    <div>Carrito</div>
+    <main className='contenedor'>
+        <h1 className='heading'>Carrito de Compras</h1>
+
+        <div className="contenido">
+            <div className="carrito">
+                <h2>Articulos</h2>
+            </div>
+
+            <aside className="resumen">
+                <h3>Resumen del Pedido</h3>
+            <p>Total a pagar: $</p>
+        </aside>
+        </div>
+    </main>
   )
 }
 
