e81086b29e37e51e4818b5f785dfd7190a6645a6
Jesus Roman <jesus_fime@outlook.es>
2023-11-01 19:01:19 -0600

    Adding rout for shopping cart and css styles update, nav bar update

--- app/components/navegacion.jsx
+++ app/components/navegacion.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link, useLocation } from '@remix-run/react'
+import imagen from '../../public/img/carrito.png'
 
 function Navegacion() {
     const location = useLocation();
@@ -9,6 +10,7 @@ function Navegacion() {
         <Link to='/nosotros' className={location.pathname === '/nosotros' ? 'active' : ''}>Nosotros</Link>
         <Link to='/guitarras' className={location.pathname === '/guitarras' ? 'active' : ''}>Tienda</Link>
         <Link to='/blog' className={location.pathname === '/blog' ? 'active' : ''}>Blog</Link>
+        <Link to='/carrito'><img src={imagen} alt='carrito de compras' /></Link>
     </nav>
   )
 }
