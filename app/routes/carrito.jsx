fda74b655ef8134eaa8944405ef42522af2dc2f4
Jesus Roman <jesus_fime@outlook.es>
2023-11-07 13:01:02 -0600

    Iteration upon carrito, root via context sended carrito to carrito.jsx

--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from '../styles/carrito.css'
+import { useOutletContext } from '@remix-run/react'
 
 export function links(){
     return[
@@ -15,6 +16,8 @@ export function meta(){
 }
 
 function Carrito() {
+    const {carrito} = useOutletContext();
+    // console.log(carrito);
   return (
     <main className='contenedor'>
         <h1 className='heading'>Carrito de Compras</h1>
@@ -22,6 +25,25 @@ function Carrito() {
         <div className="contenido">
             <div className="carrito">
                 <h2>Articulos</h2>
+
+                {carrito?.length === 0 ? 'Carrito Vacio' : (
+                    carrito.map(producto => (
+                        <div key={producto.id} className='producto'>
+                            <div>
+                                <img src={producto.imagen} alt="Imagen del producto" />
+                            </div>
+
+                            <div>
+                                <p className="nombre">{producto.nombre}</p>
+                                <p className="nombre">Cantidad: {producto.cantidad}</p>
+
+
+                                <p className='precio'>$ <span>{producto.precio}</span></p>
+                                <p className='precio'>Subtotal: $ <span>{producto.cantidad * producto.precio}</span></p>
+                            </div>
+                        </div>
+                    ))
+                )}
             </div>
 
             <aside className="resumen">
