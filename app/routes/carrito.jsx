0ca0a185c7ac301a7fca15cedfbadb2cf6db9e68
Jesus Roman <jesus_fime@outlook.es>
2023-11-07 14:39:34 -0600

    Check Total and Delete functions

--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from '../styles/carrito.css'
 import { useOutletContext } from '@remix-run/react'
 
@@ -16,8 +16,14 @@ export function meta(){
 }
 
 function Carrito() {
-    const {carrito, actualizarCantidad} = useOutletContext();
+    const [total, setTotal] = useState(0);
+    const {carrito, actualizarCantidad, eliminarGuitarra} = useOutletContext();
     // console.log(carrito);
+
+    useEffect(()=>{
+        const calculoTotal = carrito.reduce((total, producto)=> total + (producto.cantidad * producto.precio), 0)
+        setTotal(calculoTotal);
+    }, [carrito])
   return (
     <main className='contenedor'>
         <h1 className='heading'>Carrito de Compras</h1>
@@ -51,6 +57,8 @@ function Carrito() {
                                 <p className='precio'>$ <span>{producto.precio}</span></p>
                                 <p className='subtotal'>Subtotal: $ <span>{producto.cantidad * producto.precio}</span></p>
                             </div>
+
+                            <button type='button' className='btn_eliminar' onClick={() => eliminarGuitarra(producto.id)}>X</button>
                         </div>
                     ))
                 )}
@@ -58,7 +66,7 @@ function Carrito() {
 
             <aside className="resumen">
                 <h3>Resumen del Pedido</h3>
-            <p>Total a pagar: $</p>
+            <p>Total a pagar: ${total}</p>
         </aside>
         </div>
     </main>
